9b028d4a8354bf7de8633404f2202d478be208d5
Akihiro Ono <akihiro.ase@gmail.com>
2014-02-02 16:00:32 +0900

    Version 0.0.2

--- init.rb
+++ init.rb
@@ -9,6 +9,6 @@ Redmine::Plugin.register :redmine_per_project_formatting do
   name 'Redmine per project formatting plugin'
   author 'Akihiro Ono'
   description 'Redmine plugin for per-project text formatting'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/a-ono/redmine_per_project_formatting'
 end
